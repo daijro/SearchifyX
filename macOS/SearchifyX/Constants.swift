7b2a24873cf95ac26ad438f8923058db60f039ac
Jose <48543854+LavamasterYT@users.noreply.github.com>
2022-09-06 23:12:31 -0500

    Add settings, and shortcuts

---
+++ macOS/SearchifyX/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SearchifyX
+//
+//  Created by Jose Molina on 9/6/22.
+//
+
+import Foundation
+import KeyboardShortcuts
+
+extension KeyboardShortcuts.Name {
+    static let openSearchify = Self("openSearchify")
+}
